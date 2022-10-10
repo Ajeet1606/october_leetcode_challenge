963c04480e93e8794001e7050c6aa882adc7ec87
Ajeet Patel <patelajeet1606@gmail.com>
2022-10-10 15:35:08 +0530

    Break a plaindrome problem added

---
+++ Day10.cpp
@@ -0,0 +1,56 @@
+//Before middle point of string, we try to make non 'a' chars to 'a', if not possible, make the last one to 'b'.
+
+
+
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+        int n = palindrome.length();
+        if(n == 1) return "";
+        int l = 0, r = n-1;
+        bool done = false;
+        while(l <= r and !done){
+            if(l < r){
+                if(palindrome[l] != 'a'){
+                    palindrome[l] = 'a';
+                    done = true;
+                }
+            }else{
+                if(palindrome[l] != 'a' and palindrome[l-1] != 'a'){
+                    palindrome[l] = 'a';
+                    done = true;
+                }
+            }
+            l++;
+            r--;
+        }
+        if(!done){
+            palindrome[n-1] = 'b';
+        }
+
+        return palindrome;
+    }
+};
+
+
+
+<<<<<<<<<<<<<<<<<-----------Better way----------------->>>>>>>>>>>>>>>>>>>>>>>>
+
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+        int n = palindrome.length();
+
+        int i=0;
+        while(i < n/2){
+            if(palindrome[i] != 'a'){
+                palindrome[i] = 'a';
+                return palindrome;
+            }
+            i++;
+        }
+
+        palindrome[n-1] = 'b';
+        return n<2? "": palindrome;
+    }
+};
